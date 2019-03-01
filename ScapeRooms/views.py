114abbae58f0d7ce8d5b12056ad7e5939ee548a2
raultds <rrj1@alumnes.udl.cat>
2019-03-01 09:43:32 +0100

    Afegit coses

--- ScapeRooms/views.py
+++ ScapeRooms/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import DetailView
+
+from ScapeRooms.models import ScapeRoom
+
+
+class ScapeRoomDetail(DetailView):
+    model = ScapeRoom
+    template_name = 'scapeRoom/scapeRoom_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ScapeRoomDetail, self).get_context_data(**kwargs)
+        context['RATING CHOICES'] = ScapeRoomDetail.RATING_CHOICES
+
